b695da78fbdf322eced019ed98c4bfeaf95a995c
hoangdn1908 <hoangtranb02@gmail.com>
2026-02-03 17:58:36 +0700

    Update SortStudentByMarks.java

--- src/Week4/SortStudentByMarks.java
+++ src/Week4/SortStudentByMarks.java
@@ -7,8 +7,7 @@ public class SortStudentByMarks {
      * Constructor for objects of class Exercise02
      */
     public SortStudentByMarks() {
-        /// The ngu
-    }
+        }
 
     public static void printStudentList(String[] names, int[] marks) {
         int n = marks.length;
