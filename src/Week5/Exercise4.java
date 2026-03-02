6fcb3a5f2605c6cd9014a01430de2bc06931279e
hoangdn1908 <hoangtranb02@gmail.com>
2026-03-02 13:38:11 +0700

    update ex4

--- src/Week5/Exercise4.java
+++ src/Week5/Exercise4.java
@@ -1,7 +1,5 @@
 package Week5;
 
-import java.util.Arrays;
-import java.util.OptionalInt;
 import java.util.Scanner;
 
 public class Exercise4 {
@@ -14,9 +12,9 @@ public class Exercise4 {
     public static void printStudentList(String[] names, int[] marks) {
         int n = marks.length;
         System.out.println();
-        System.out.println("The list of " + n +" students is");
-        for (int i=0; i<n; i++)
-            System.out.println("Name: "+names[i]+"  Mark: "+ marks[i]);
+        System.out.println("The list of " + n + " students is");
+        for (int i = 0; i < n; i++)
+            System.out.println("Name: " + names[i] + "  Mark: " + marks[i]);
         System.out.println();
     }
 
@@ -32,38 +30,36 @@ public class Exercise4 {
         int[] output_marks = new int[n];
         // QUESTION 4
         // Write java code to: Initialize count array with all zeros;
-//       for(int i = 0; i < count.length; i++)
-//       {
-//           count[i] = 0;
-//       }
+//        for(int i = 0; i < count.length; i++)
+//        {
+//            count[i] = 0;
+//        }
         // Store the count of each element;
         for(int mark : marks)
         {
             count[mark]++;
         }
         // Calculate the cumulative count of each array
-        for(int i = 9; i >= 0; i--)
+        for(int i = count.length - 2; i >= 0; i--)
         {
             count[i] += count[i + 1];
         }
         // End of QUESTION 4
+
         // QUESTION 5
         // Write java code to find the index of each element of the original array in count array,
         for(int i = n - 1; i >= 0; i--)
         {
             int mark = marks[i];
-            int position = count[mark] - 1;
+            int index = count[mark] - 1;
             // place the elements in output array;
-            output_marks[position] = mark;
-            output_names[position] = names[i];
+            output_marks[index] = marks[i];
+            output_names[index] = names[i];
             count[mark]--;
         }
         // Copy the sorted elements into original array
-        for(int i = 0; i < n; i++)
-        {
-            marks[i] = output_marks[i];
-            names[i] = output_names[i];
-        }
+        System.arraycopy(output_marks, 0, marks, 0, n);
+        System.arraycopy(output_names, 0, names, 0, n);
         // End of QUESTION 5
     }
 
@@ -71,14 +67,13 @@ public class Exercise4 {
      * This method shows the first M students from the sorted list
      */
     public static void showStudents(String[] names, int[] marks, int m) {
-        System.out.println("The list of " + m +" students who has the highest mark is:");
-        for (int i=0; i<m; i++)
-            System.out.println("Name: "+names[i]+"  Mark: "+ marks[i]);
+        System.out.println("The list of " + m + " students who has the highest mark is:");
+        for (int i = 0; i < m; i++)
+            System.out.println("Name: " + names[i] + "  Mark: " + marks[i]);
     }
 
-    public static void main(String[]args)
-    {
-        int n,m;
+    public static void main(String[] args) {
+        int n, m;
         int[] marks;
         String[] names;
 
@@ -89,13 +84,13 @@ public class Exercise4 {
         m = scanner.nextInt();
 
         //Remove newline character after calling nextInt()
-        String tmp=scanner.nextLine();
+        String tmp = scanner.nextLine();
 
         marks = new int[n];
         names = new String[n];
 
-        for (int i=0; i<n; i++) {
-            System.out.println("Please input the name and mark of student "+ (i+1) +":");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Please input the name and mark of student " + (i + 1) + ":");
             System.out.println("Name: ");
             names[i] = scanner.nextLine();
             System.out.println("Mark: ");
@@ -104,9 +99,8 @@ public class Exercise4 {
             tmp = scanner.nextLine();
         }
 
-        printStudentList(names,marks);
-        sortStudentByMark(names,marks);
-        showStudents(names,marks,m);
+        printStudentList(names, marks);
+        sortStudentByMark(names, marks);
+        showStudents(names, marks, m);
     }
 }
-
